010543b944473d21f3258839033a20129a2807f8
Autumn Kangas <autumnkangas@gmail.com>
2018-03-09 20:03:54 -0600

    add show action to author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,23 @@
 class AuthorsController < ApplicationController
+
+  before_action :find_author
+
+  def show
+
+  end
+
+  private
+
+  def find_author
+    @author = Author.find_by(id: params[:id])
+    @authors = find_books(@author)
+    if !@author
+      redirect_to authenticated_root_path
+    end
+  end
+
+  def find_books(author)
+    Book.find_books_by_author(author, current_user)
+  end
   
 end
