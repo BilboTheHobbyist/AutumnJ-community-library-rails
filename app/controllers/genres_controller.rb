af0c6fe4fd12c90d7d3fcfdc9c2a01e01e8fa1bd
Autumn Kangas <autumnkangas@gmail.com>
2018-03-09 20:04:13 -0600

    add show action to genre controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,23 @@
 class GenresController < ApplicationController
 
+  before_action :find_genre
+
+  def show
+
+  end
+
+  private
+
+  def find_genre
+    @genre = Genre.find_by(id: params[:id])
+    @books = find_books(@genre)
+    if !@genre
+      redirect_to authenticated_root_path
+    end
+  end
+
+  def find_books(genre)
+    Book.find_books_by_genre(genre, current_user)
+  end
 
 end
