4b4b26a999f42e081a5a097d55afd238f94b6a28
Autumn Kangas <autumnkangas@gmail.com>
2018-03-09 19:03:43 -0600

    class level helper methods added to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,16 @@ class Comment < ActiveRecord::Base
 
   validates :content, presence: true
 
+  def self.user_comments(current_user)
+    where({user_id: current_user.id})
+  end
+
+  def self.user_comments_by_book(book_id, current_user)
+    where({user_id: current_user.id, book_id: book_id}).order(:created_at)
+  end
+
+  def self.all_comments(book_id)
+    where({book_id: book_id}).order(:created_at)
+  end
+
 end
