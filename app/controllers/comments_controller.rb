63d76d0df948e6352dc3eaec252762ace46a3ce6
Autumn Kangas <autumnkangas@gmail.com>
2018-03-09 19:05:17 -0600

    style updates to keep things looking consistent

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,12 @@
 class CommentsController < ApplicationController
 
-  before_action :find_book
+  before_action :find_book, :find_comment
+  skip_before_action :find_comment, :only => [:new, :create, :index]
+  skip_before_action :find_book, :only => [:index]
+
+  def index
+    @comments = Comment.user_comments(current_user)
+  end
 
   def new
     @comment = Comment.new
@@ -8,25 +14,51 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    @comment.created_at = Time.now
     @comment.user_id = current_user.id #should this be hidden field in form?
-    if @comment.save 
+    if @comment.save
       redirect_to borrowed_book_path(@book) 
     else
       render :new
     end
   end 
 
+  def edit
+
+  end
+
+  def update
+    if @comment.update(comment_params) 
+      if @book.borrower == current_user.id
+        redirect_to borrowed_book_path(@book)
+      else
+        redirect_to comments_path
+      end
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @comment.delete
+    redirect_to borrowed_book_path(@book), {notice: 'The comment was removed.'}
+  end
+
   private
 
   def find_book
-     @book = Book.find_by(id: params[:book_id], borrower: current_user.id)
+    @book = Book.find_by(id: params[:book_id])
+  end
+
+  def find_comment
+    @comment = Comment.find_by(id: params[:id], user_id: current_user.id, book_id: params[:book_id])
+    if @comment.nil?
+      redirect_to borrowed_book_path(@book), {notice: 'Please select a comment below to update or remove.'}
+    end
   end
 
   def comment_params
     params.require(:comment).permit(
       :book_id, 
-      # :user_id, 
       :content
       )
   end
