400c516decb4a713d79d4ed56dc2fbb970e0c28e
Autumn Kangas <autumnkangas@gmail.com>
2018-03-19 13:52:43 -0500

    clean up validations

--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -3,7 +3,6 @@ class BookAuthor < ActiveRecord::Base
   belongs_to :book
   belongs_to :author 
 
-  validates :author_id, uniqueness: {scope: :book_id, message: "author is already associataed with"}
-  validates :author, presence: true, if: -> { author_id.present? }
+  validates :author_id, uniqueness: {scope: :book_id}
 
 end
