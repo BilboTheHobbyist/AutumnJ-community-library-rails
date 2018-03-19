51a8ea05700dba5b9f528ebdaabba1d88d14101c
Autumn Kangas <autumnkangas@gmail.com>
2018-03-19 13:34:37 -0500

    troubleshoot error validation display on forms by testing validations

--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -3,6 +3,7 @@ class BookAuthor < ActiveRecord::Base
   belongs_to :book
   belongs_to :author 
 
-  validates :author_id, uniqueness: {scope: :book_id}
-  
+  validates :author_id, uniqueness: {scope: :book_id, message: "author is already associataed with"}
+  validates :author, presence: true, if: -> { author_id.present? }
+
 end
