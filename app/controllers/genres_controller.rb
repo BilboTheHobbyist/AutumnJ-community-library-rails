73a94d498e1dc5571f009df93b6048f6a7135b99
Autumn Kangas <autumnkangas@gmail.com>
2018-03-20 15:17:14 -0500

    add borrowable books display to genre show feature, powered by controller methods

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,6 @@
 class GenresController < ApplicationController
 
-  before_action :find_genre
+  before_action :find_genres_books, :find_genres_borrowable_books
 
   def show
 
@@ -8,12 +8,24 @@ class GenresController < ApplicationController
 
   private
 
-    def find_genre
-    @genre = Genre.find_by(id: params[:id])
+  def find_genres_books
+    @genre = find_genre
     @books = @genre.find_genres_books(current_user)
     if !@genre
       redirect_to authenticated_root_path
     end
+  end
+
+  def find_genres_borrowable_books
+    @genre = find_genre
+    @borrowable_books = @genre.find_genres_borrowable_books(current_user)
+    if !@genre
+      redirect_to authenticated_root_path
+    end
+  end
+
+  def find_genre
+    Genre.find_by(id: params[:id])
   end
 
 end
