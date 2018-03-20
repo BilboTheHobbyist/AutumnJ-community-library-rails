02fbf3dbcbddc7b72c0930655854e7d57362601f
Autumn Kangas <autumnkangas@gmail.com>
2018-03-20 01:06:26 -0500

    remove commented code from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_filter :set_cache_buster
+  before_action :set_cache_buster
 
   private 
   
@@ -12,8 +12,6 @@ class ApplicationController < ActionController::Base
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
-
-  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource_or_scope)
     user_session_path
