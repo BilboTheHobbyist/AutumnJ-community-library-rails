f23050289a6aee7a45866257085e34577680d77e
Autumn Jung <autumnkangas@gmail.com>
2018-12-26 21:13:20 -0600

    resolve activejob vulnerability (#2)

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '= 5.0.2'
+gem 'rails'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
@@ -32,6 +32,7 @@ gem 'devise', '~> 4.2.0'
 gem 'dotenv-rails'
 gem 'omniauth-github'
 
+gem 'activejob', '= 5.0.7.1'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,17 @@ module AutumnJRailsCommunityLibrary
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # HEADS UP! i18n 1.1 changed fallbacks to exclude default locale.
+    # But that may break your application.
+
+    # Please check your Rails app for 'config.i18n.fallbacks = true'.
+    # If you're using I18n (>= 1.1.0) and Rails (< 5.2.2), this should be
+    # 'config.i18n.fallbacks = [I18n.default_locale]'.
+    # If not, fallbacks will be broken in your app by I18n 1.1.x.
+
+    # For more info see:
+    # https://github.com/svenfuchs/i18n/releases/tag/v1.1.0
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
