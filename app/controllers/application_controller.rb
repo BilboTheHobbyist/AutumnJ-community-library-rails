3b49e2d9322f66dceab4786581833abdafaceff0
BilboTheHobbyist <nicolas.gondard@mines-nancy.org>
2020-11-20 15:52:38 +0100

    as per https://www.synbioz.com/blog/tech/le-kit-du-bon-developpeur-rails-partie-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale]
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
