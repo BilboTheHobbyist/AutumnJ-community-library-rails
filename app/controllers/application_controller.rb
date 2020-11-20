051c2d386b9723b17a107f2a3943fc93e051d43f
BilboTheHobbyist <nicolas.gondard@mines-nancy.org>
2020-11-20 18:26:25 +0100

    as per https://phrase.com/blog/posts/rails-i18n-guide/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale]
+    I18n.locale = params[:locale]|| I18n.default_locale
   end
 
   # Prevent CSRF attacks by raising an exception.
